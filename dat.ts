c22f586956da47a4653b9791c2029a6e7660c71f
jyand <jyand@protonmail.com>
2021-01-27 23:16:59 -0500

    init

---
+++ dat.ts
@@ -0,0 +1,8 @@
+export const buttons: string[] = ["Add to Prompt", "Delete", "Clear All", "New Line"] ;
+export const colorNames: string[] = [] ;
+export const colorValues: string[] = [] ;
+export const envNames: string[] = [] ;
+export const envValues: string[] = [] ;
+export const utfChars: string[] = [] ;
+export const utfValues: string[] = [] ;
+// create Map<string, string>()s in main
